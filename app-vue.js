dcb0fe582846b5751a41d81eff486467a8570827
GalisGalisteo <galis.travelling@gmail.com>
2023-11-13 13:02:53 +0100

    starting exercice

---
+++ app-vue.js
@@ -0,0 +1,11 @@
+import {
+    createApp,
+    ref,
+    computed,
+  } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+  
+  createApp({
+    setup() {
+      return {};
+    },
+  }).mount("#app");
